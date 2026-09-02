99cb6c733631c4717d6c51dfd46f69a56ca6b8b3
vipincd <vipincd@gmail.com>
2026-09-02 20:33:38 +0200

    chore: initialize Samgamam mobile app

--- src/hooks/use-color-scheme.web.ts
+++ src/hooks/use-color-scheme.web.ts
@@ -1,15 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useSyncExternalStore } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
+
+const subscribeToHydration = () => () => {};
+const getClientHydrationSnapshot = () => true;
+const getServerHydrationSnapshot = () => false;
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
-  const [hasHydrated, setHasHydrated] = useState(false);
-
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
+  const hasHydrated = useSyncExternalStore(
+    subscribeToHydration,
+    getClientHydrationSnapshot,
+    getServerHydrationSnapshot
+  );
 
   const colorScheme = useRNColorScheme();
 
